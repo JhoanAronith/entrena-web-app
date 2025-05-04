f624b09ecc760ba4e0b93d4127c84167661cd2c8
Jhoan <jhoanmunozman@gmail.com>
2025-05-04 13:34:31 -0500

    chore: commit inicial del proyecto vacío

---
+++ src/main/java/com/Proyecto/Entrena/EntrenaApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Entrena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto/Entrena/EntrenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Entrena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
